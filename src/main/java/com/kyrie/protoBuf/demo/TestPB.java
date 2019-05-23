4ebba7fc309aaf564d1912396b3d3e507be0395b
luyizhizaio <lichangyue528@sina.com>
2019-05-23 22:28:20 +0800

    protobuf demo

---
+++ src/main/java/com/kyrie/protoBuf/demo/TestPB.java
@@ -0,0 +1,28 @@
+package com.kyrie.protoBuf.demo;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.kyrie.protoBuf.demo.AddressBookProtos.Person;
+
+/**
+ * Created by Kyrie on 2019/5/23.
+ */
+public class TestPB {
+
+    public static void main(String args[]) throws InvalidProtocolBufferException {
+
+        Person person = Person.newBuilder()
+                .setEmail("")
+                .setId(122)
+                .setName("xxx").build();
+
+        System.out.println(person.toString());
+
+        //对象转成字节数组
+        byte[] bytes = person.toByteArray();
+        Person person2 = Person.parseFrom(bytes);
+
+        System.out.println(person2);
+    }
+
+
+
+}
