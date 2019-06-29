40edbe66acdfcbc68945b585a36141f4f2efb77a
luyizhizaio <lichangyue528@sina.com>
2019-06-29 18:19:44 +0800

    discard

---
+++ src/main/java/com/kyrie/discard/DiscardServerHandler.java
@@ -0,0 +1,34 @@
+package com.kyrie.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by Kyrie on 2019/6/29.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+
+    /**
+     * This method is called with the received message,
+     * @param ctx
+     * @param msg
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // Discard the received data silently.
+        ((ByteBuf) msg).release(); // (3)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+
+
+}
